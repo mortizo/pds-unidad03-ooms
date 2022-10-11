79558e4dff3f20f7b9efd188bf56b26380144bc9
mortizo <mortizo@ups.edu.ec>
2022-10-11 17:56:11 -0500

    State Interface

---
+++ pds-unidad03-ooms/src/main/java/p61/state/EstadoMatricula.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package p61.state;
+
+/**
+ *
+ * @author morti
+ */
+public interface EstadoMatricula {
+    
+    public boolean pendiente();
+    public boolean pagadoParcial();
+    public boolean pagadoTotal();
+    
+}
